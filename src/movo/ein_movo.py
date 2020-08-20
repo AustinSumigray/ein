dd2b49099558d9253e12418b113c3bb42d48f59b
Austin Sumigray <AustinSumigray@Gmail.com>
2020-08-19 20:02:21 -0400

    Update to add movement

--- src/movo/ein_movo.py
+++ src/movo/ein_movo.py
@@ -4,6 +4,7 @@ import rospy
 import moveit_commander
 import moveit_msgs.msg
 import geometry_msgs.msg
+from movo_msgs.msg import ConfigCmd, LinearActuatorCmd
 import actionlib
 import threading
 from math import pi
@@ -11,6 +12,8 @@ from std_msgs.msg import String
 from moveit_commander.conversions import pose_to_list
 from control_msgs.msg import GripperCommandAction, GripperCommandGoal
 from geometry_msgs.msg import PoseStamped, Twist
+from movo_action_clients.torso_action_client import TorsoActionClient
+from sensor_msgs.msg import JointState
 
 # Create the action client for the grippers, this will send commands
 # to the movo gripper topics
@@ -18,6 +21,8 @@ rightGripperActionClient = actionlib.SimpleActionClient(
   "/movo/right_gripper_controller/gripper_cmd", GripperCommandAction)
 leftGripperActionClient = actionlib.SimpleActionClient(
   "/movo/left_gripper_controller/gripper_cmd", GripperCommandAction)
+
+TRACTOR_REQUEST = 5
 
 # Initialize the connection to the moveit robot
 moveit_commander.roscpp_initialize(sys.argv)
@@ -47,7 +52,8 @@ class ein_movo:
       'closeGripper': [self.moveGripper,[leftGripperActionClient, 0.01]], 
       'openGripper': [self.moveGripper,[leftGripperActionClient, 0.165]],
       'moveToEEPose': [self.moveToEEPose,[leftArm]],
-      'twistMove': [self.twistMove, []]
+      'twistMove': [self.twistMove, []],
+      'torsoMove': [self.torsoMove, []]
     }
     words = data.split()
     for i in range(len(words)):
@@ -63,7 +69,8 @@ class ein_movo:
       'closeGripper': [self.moveGripper,[rightGripperActionClient, 0.01]],
       'openGripper': [self.moveGripper,[rightGripperActionClient, 0.165]],
       'moveToEEPose': [self.moveToEEPose,[rightArm]],
-      'twistMove': [self.twistMove, []]
+      'twistMove': [self.twistMove, []],
+      'torsoMove': [self.torsoMove, []]
     }
     words = data.split()
     for i in range(len(words)):
@@ -122,11 +129,40 @@ class ein_movo:
 
   # Moves the base of the movo
   def twistMove(self, data, i, incomingArgs):
-    twister = threading.Thread(target=self.twistThread, args=(data, i, incomingArgs))
-    twister.start()
+    # twister = threading.Thread(target=self.twistThread, args=(data, i, incomingArgs))
+    # twister.start()
+    acquired = self.movingLock.acquire(not self.moving)
+    if acquired:
+      self.moving = True
+      twist = Twist()
+      
+      twist.linear.x = float(data[i - 6])
+      twist.linear.y = float(data[i - 5])
+      twist.linear.z = float(data[i - 4])
+      twist.angular.x = float(data[i - 3])
+      twist.angular.y = float(data[i - 2])
+      twist.angular.z = float(data[i - 1])
+      print("Twist: " + str(twist))
+      self.twistPub.publish(twist)
+      self.movingLock.release()
+      self.moving = False
+    else:
+      pass
     return "Twist Move"
 
-  # Expecting this to come in the form 6 numbers and then twistMove
+  # Moves the torso of the movo up and down
+  def torsoMove(self, data, i, args):
+    
+    torso_change = float(data[i - 1])
+    # Wait for the current state of the torso to come in
+    temp_torso = rospy.wait_for_message("/movo/linear_actuator/joint_states", JointState)
+    current_torso_pos = list(temp_torso.position)
+    torsoCmd = LinearActuatorCmd()
+    torsoCmd.header.stamp = rospy.get_rostime()
+    torsoCmd.desired_position_m = current_torso_pos + .01 * torso_change
+    torsoCmd.fdfwd_vel_mps = 0
+    self.torsoPub.publish(torsoCmd)
+
   def twistThread(self ,data, i, args):
     acquired = self.movingLock.acquire(not self.moving)
     if acquired:
@@ -139,22 +175,44 @@ class ein_movo:
       twist.angular.x = float(data[i - 3])
       twist.angular.y = float(data[i - 2])
       twist.angular.z = float(data[i - 1])
-
+      print("Twist: " + str(twist))
       self.twistPub.publish(twist)
       self.movingLock.release()
       self.moving = False
     else:
       pass
 
+  # Enable the base of the movo to move
+  def turnBaseOn(self):
+    configMessage = ConfigCmd()
+    configMessage.gp_cmd = 'GENERAL_PURPOSE_CMD_SET_OPERATIONAL_MODE'
+    configMessage.gp_param = TRACTOR_REQUEST
+    configMessage.header.stamp = rospy.get_rostime()
+    self.configPub.publish(configMessage)
+  
+  # Disable the base of the movo from moving
+  def turnBaseOff(self):
+    configMessage = ConfigCmd()
+    configMessage.gp_cmd = 'GENERAL_PURPOSE_CMD_NONE'
+    configMessage.gp_param = 0
+    configMessage.header.stamp = rospy.get_rostime()
+    self.configPub.publish(configMessage)
+
   def listener(self):
     rospy.init_node('movo_python')
     # Defines whether the robot is currently acting out a plan
     self.movingLock = threading.Lock()
     self.moving = False
+    self.movo_torso = TorsoActionClient()
 
     rospy.Subscriber("ein/right/forth_commands", String, self.movo_commands_right)
     rospy.Subscriber("ein/left/forth_commands", String, self.movo_commands_left)
     self.twistPub = rospy.Publisher("/movo/cmd_vel", Twist, queue_size=10)
+    self.configPub = rospy.Publisher("/movo/gp_command", ConfigCmd, queue_size=10)
+    self.torsoPub = rospy.Publisher("/movo/linear_actuator_cmd", LinearActuatorCmd, queue_size=10);
+
+    # Configure the robot to start moving (Later move this to a toggle)
+    self.turnBaseOn()
 
     # Don't exit
     rospy.spin()
